cf0b6e1a0d74700430e9d25cbf9c9d2f88b20c9b
Jerry Lee <jerry.lee@nexclipper.io>
2022-04-18 16:53:38 -0700

    add node logic

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const RABBITMQ_SERVER_PORT = process.env.RABBITMQ_SERVER_PORT || 5672;
 const RABBITMQ_SERVER_QUEUE_RESOURCE = process.env.RABBITMQ_SERVER_QUEUE_RESOURCE || "lari_resource";
 const RABBITMQ_SERVER_QUEUE_ALERT = process.env.RABBITMQ_SERVER_QUEUE_ALERT || "lari_resource";
 const RABBITMQ_SERVER_QUEUE_METRIC = process.env.RABBITMQ_SERVER_QUEUE_METRIC || "lari_metric";
+const NODE_EXPORTER_PORT = process.env.NODE_EXPORTER_PORT || 9090;
+
 
 const amqp = require("amqplib");
 const axios = require('axios');
@@ -89,6 +91,49 @@ async function connectQueue() {
 
                     case "00000000000000000000000000000010":  //10, for K8s nodes
 
+                    for (var i=0; i<itemLength; i++)
+                    {
+                        // get internal IP address from addresses array and assign to InternalIP variable.
+                        internalIpLength = result.items[i].status.addresses.length
+                        for (var j=0; j<internalIpLength; j++)
+                        {
+                            if (result.items[i].status.addresses[j].type = 'InternalIP')
+                            { 
+                                internalIp = result.items[i].status.addresses[j].address;
+                            }
+                        }
+                        
+                        query['cluster_uuid'] = TotalMsg.cluster_uuid ;  
+                        query['resource_Name'] = result.items[i].metadata.name ;
+                        query['resource_Labels'] = result.items[i].metadata.labels ;
+                        query['resource_Annotations'] = result.items[i].metadata.annotations ;
+                        //query['resource_Namespace'] = result.items[i].metadata.namespace; 
+                        query['resource_Instance'] = internalIp + ":" + NODE_EXPORTER_PORT;
+                        query['resource_Status'] = result.items[i].status;
+                        query['resource_Type'] = "ND";
+                        query['resource_Level1'] = "K8";
+                        query['resource_Level2'] = "ND";
+                        //query['resource_Level3'] = "SV";
+                        query['resource_Level_Type'] = "KN";
+                        query['resource_Rbac'] = "true";
+                        query['resource_Anomaly_Monitor'] = "true";
+                        query['resource_Active'] = "true";
+                        query['resource_Status_Updated_At'] = new Date();
+
+                        if (i==0) {
+                            mergedQuery = '{"node":[' + JSON.stringify(query);
+                            
+                        }
+                        else if (i==(itemLength-1)) {
+                            mergedQuery = mergedQuery + "," + JSON.stringify(query) + "]}";
+                            API_MSG =JSON.parse(mergedQuery);
+                            console.log(API_MSG);
+                        }
+                        else {
+                            mergedQuery = mergedQuery +  "," + JSON.stringify(query);
+                        }
+                    }
+             
                     break;
 
                     case "00000000000000000000000000000004":  //04, for K8s namespaces
