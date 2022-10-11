0228f1d9f6cb0291bbfe1f6e34d3ec3591c5dbc2
Jerry Lee <jerry.lee@nexclipper.io>
2022-10-11 16:20:38 -0700

    change back to nodemon

--- index.js
+++ index.js
@@ -66,6 +66,23 @@ const vm_Url = process.env.VM_URL;
 const VM_MULTI_AUTH_URL = process.env.VM_MULTI_AUTH_URL;
 const VM_OPTION = process.env.VM_OPTION; //BOTH - both / SINGLE - single-tenant / MULTI - multi-tenant
 
+process.stdin.resume();//so the program will not close instantly
+function exitHandler(options, exitCode) {
+    if (options.cleanup) console.log('clean');
+    if (exitCode || exitCode === 0) console.log(exitCode);
+    if (options.exit) process.exit();
+}
+//do something when app is closing
+process.on('exit', exitHandler.bind(null,{cleanup:true}));
+//catches ctrl+c event
+process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+// catches "kill pid" (for example: nodemon restart)
+process.on('SIGUSR1', exitHandler.bind(null, {exit:true}));
+process.on('SIGUSR2', exitHandler.bind(null, {exit:true}));
+//catches uncaught exceptions
+process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
+
+
 connectQueue()
 
 async function connectQueue() {
@@ -1021,7 +1038,7 @@ async function callVM (metricReceivedMassFeed, clusterUuid) {
             throw error;
         };
     }
-    return result;  
+    return result;
 }
 
 app.listen(MQCOMM_PORT, () => console.log("NexClipper MQCOMM Server running at port " + MQCOMM_PORT));
