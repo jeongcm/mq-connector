135535bf2480f619e2deab12e3fb7d939cbab4ed
Jerry Lee <jerry.lee@nexclipper.io>
2022-10-11 20:22:40 -0700

    console log

--- index.js
+++ index.js
@@ -930,6 +930,7 @@ async function massUploadMetricReceived(metricReceivedMassFeed, clusterUuid){
         receivedData = null;
         const message_size_mb = (Buffer.byteLength(JSON.stringify(receivedMetrics)))/1024/1024;
         console.log (`2. metric received name: ${name}, message size: ${message_size_mb}` );
+
         if (message_size_mb>5){
           const half = Math.ceil(receivedMetrics.length/2);
           const firstHalf = receivedMetrics.slice(0, half); 
@@ -941,6 +942,7 @@ async function massUploadMetricReceived(metricReceivedMassFeed, clusterUuid){
           });
           let finalResult1 = (newResultMap1).join("\n")
           newResultMap1 = null;
+          console.log ("final result1:", finalResult1);
           let massFeedResult1 = await callVM(finalResult1, clusterUuid);
           console.log(`3-1. massFeedResult 1/2: ${massFeedResult1.status}, clusterUuid: ${clusterUuid}, name: ${name}`); 
           finalResult1=null;
@@ -951,6 +953,7 @@ async function massUploadMetricReceived(metricReceivedMassFeed, clusterUuid){
           });
           let finalResult2 = (newResultMap2).join("\n")
           newResultMap2= null;
+          console.log ("final result2:", finalResult1);
           let massFeedResult2 = await callVM(finalResult2, clusterUuid);
           console.log(`3-2, massFeedResult 2/2: ${massFeedResult2.status}, clusterUuid: ${clusterUuid}, name: ${name}`); 
           finalResult2=null;
