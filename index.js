9423ad45b9f304411efde2a4ae28e4b55df4b163
jeongcm <jeongcm@github.com>
2022-10-21 14:57:28 +0900

    mig/1006 PM에 대한 Resource mass 처리 제거, sudory template uuid 추가

--- index.js
+++ index.js
@@ -796,44 +796,44 @@ async function connectQueue() {
                 //
                 //     tempQuery = formatter_resource(i, itemLength, resourceType, cluster_uuid, query, mergedQuery);
                 //     API_MSG = JSON.parse(tempQuery);
-                case "PMLIST-TEMPLATE-UUID":  //TODO insert Openstack PM template uuid
-                    resourceType = "PM";
-                    console.log("------------------------------------------------------------------------")
-                    console.log("PM LIST message: ", result)
-                    console.log("------------------------------------------------------------------------")
-                    for (var i=0; i<itemLength; i++)
-                    {
-                        query['resource_Type'] = resourceType ;
-                        // query['resource_Spec'] = result.items[i].spec;
-                        query['resource_Group_Uuid'] = cluster_uuid ;
-                        // query['resource_Name'] = result.items[i].metadata.name ;
-                        // query['resource_Instance'] = result.items[i].metadata.name ; // TODO: set host ip
-                        // query['resource_Target_Uuid'] = result.items[i].metadata.uid ; // TODO: set Host uuid?
-                        // query['resource_Target_Created_At'] = result.items[i].metadata.creationTimestamp ;
-                        // query['resource_Labels'] = result.items[i].metadata.labels ; //object
-                        // query['resource_Annotations'] = result.items[i].metadata.annotations ; //object
-                        // query['resource_Owner_References'] = result.items[i].metadata.ownerReferences ; //object
-                        // query['resource_Sc_Provisioner'] = result.items[i].provisioner;
-                        // query['resource_Sc_Reclaim_Policy'] = result.items[i].reclaimPolicy;
-                        // query['resource_Sc_Allow_Volume_Expansion'] = result.items[i].allowVolumeExpansion;
-                        // query['resource_Sc_Volume_Binding_Mode'] = result.items[i].volumeBindingMode;
-                        // query['resource_Status'] = result.items[i].status; //object
-                        query['resource_Level1'] = "OS"; //Openstack
-                        query['resource_Level2'] = resourceType;
-                        query['resource_Level_Type'] = "OX";  //Openstack-Cluster
-                        query['resource_Rbac'] = false;
-                        query['resource_Anomaly_Monitor'] = false;
-                        query['resource_Active'] = true;
-                        // query['resource_Status_Updated_At'] = new Date();
-
-                        tempQuery = formatter_resource(i, itemLength, resourceType, cluster_uuid, query, mergedQuery);
-                        mergedQuery = tempQuery;
-                    }
-
-                    API_MSG = JSON.parse(mergedQuery);
-
-                    break;
-                case "PJLIST-TEMPLATE-UUID":  //TODO insert Openstack PJ List template uuid
+                // case "PMLIST-TEMPLATE-UUID":  //TODO insert Openstack PM template uuid
+                //     resourceType = "PM";
+                //     console.log("------------------------------------------------------------------------")
+                //     console.log("PM LIST message: ", result)
+                //     console.log("------------------------------------------------------------------------")
+                //     for (var i=0; i<itemLength; i++)
+                //     {
+                //         query['resource_Type'] = resourceType ;
+                //         // query['resource_Spec'] = result.items[i].spec;
+                //         query['resource_Group_Uuid'] = cluster_uuid ;
+                //         // query['resource_Name'] = result.items[i].metadata.name ;
+                //         // query['resource_Instance'] = result.items[i].metadata.name ; // TODO: set host ip
+                //         // query['resource_Target_Uuid'] = result.items[i].metadata.uid ; // TODO: set Host uuid?
+                //         // query['resource_Target_Created_At'] = result.items[i].metadata.creationTimestamp ;
+                //         // query['resource_Labels'] = result.items[i].metadata.labels ; //object
+                //         // query['resource_Annotations'] = result.items[i].metadata.annotations ; //object
+                //         // query['resource_Owner_References'] = result.items[i].metadata.ownerReferences ; //object
+                //         // query['resource_Sc_Provisioner'] = result.items[i].provisioner;
+                //         // query['resource_Sc_Reclaim_Policy'] = result.items[i].reclaimPolicy;
+                //         // query['resource_Sc_Allow_Volume_Expansion'] = result.items[i].allowVolumeExpansion;
+                //         // query['resource_Sc_Volume_Binding_Mode'] = result.items[i].volumeBindingMode;
+                //         // query['resource_Status'] = result.items[i].status; //object
+                //         query['resource_Level1'] = "OS"; //Openstack
+                //         query['resource_Level2'] = resourceType;
+                //         query['resource_Level_Type'] = "OX";  //Openstack-Cluster
+                //         query['resource_Rbac'] = false;
+                //         query['resource_Anomaly_Monitor'] = false;
+                //         query['resource_Active'] = true;
+                //         // query['resource_Status_Updated_At'] = new Date();
+                //
+                //         tempQuery = formatter_resource(i, itemLength, resourceType, cluster_uuid, query, mergedQuery);
+                //         mergedQuery = tempQuery;
+                //     }
+                //
+                //     API_MSG = JSON.parse(mergedQuery);
+                //
+                //     break;
+                case "50000000000000000000000000000002":  //TODO insert Openstack PJ List template uuid
                     length = result.projects.length
                     if (length === 0)
                     {
@@ -866,7 +866,7 @@ async function connectQueue() {
 
                 break;
 
-                case "VMLIST-TEMPLATE-UUID":  //TODO insert Openstack VM List template uuid
+                case "50000000000000000000000000000004":  //TODO insert Openstack VM List template uuid
                     length = result.servers.length
                     if (length === 0)
                     {
@@ -898,7 +898,7 @@ async function connectQueue() {
                     API_MSG = JSON.parse(mergedQuery);
 
                 break;
-                case "VM-TEMPLATE-UUID":  //TODO insert Openstack VM template uuid
+                case "50000000000000000000000000000003":  //TODO insert Openstack VM template uuid
                     resourceType = "VM";
 
                     query['resource_Type'] = resourceType;
