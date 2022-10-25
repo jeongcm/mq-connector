384c2f5bf7af9ed076db46d9ba403cc7da8eb1c8
jeongcm <jeongcm@github.com>
2022-10-25 14:41:21 +0900

    mig/1006 잘못된 변수 입력 문제 해결

--- index.js
+++ index.js
@@ -794,30 +794,15 @@ async function connectQueue() {
                 //     query['resource_Anomaly_Monitor'] = false;
                 //     query['resource_Active'] = true;
                 //
-                //     tempQuery = formatter_resource(i, itemLength, resourceType, cluster_uuid, query, mergedQuery);
+                //     tempQuery = formatter_resource(i, length, resourceType, cluster_uuid, query, mergedQuery);
                 //     API_MSG = JSON.parse(tempQuery);
                 // case "PMLIST-TEMPLATE-UUID":  //TODO insert Openstack PM template uuid
-                //     resourceType = "PM";
-                //     console.log("------------------------------------------------------------------------")
-                //     console.log("PM LIST message: ", result)
-                //     console.log("------------------------------------------------------------------------")
-                //     for (var i=0; i<itemLength; i++)
+                //     length = result.projects.length
+                //     for (var i=0; i<length; i++)
                 //     {
                 //         query['resource_Type'] = resourceType ;
-                //         // query['resource_Spec'] = result.items[i].spec;
+                //         query['resource_Spec'] = result.items[i].spec;
                 //         query['resource_Group_Uuid'] = cluster_uuid ;
-                //         // query['resource_Name'] = result.items[i].metadata.name ;
-                //         // query['resource_Instance'] = result.items[i].metadata.name ; // TODO: set host ip
-                //         // query['resource_Target_Uuid'] = result.items[i].metadata.uid ; // TODO: set Host uuid?
-                //         // query['resource_Target_Created_At'] = result.items[i].metadata.creationTimestamp ;
-                //         // query['resource_Labels'] = result.items[i].metadata.labels ; //object
-                //         // query['resource_Annotations'] = result.items[i].metadata.annotations ; //object
-                //         // query['resource_Owner_References'] = result.items[i].metadata.ownerReferences ; //object
-                //         // query['resource_Sc_Provisioner'] = result.items[i].provisioner;
-                //         // query['resource_Sc_Reclaim_Policy'] = result.items[i].reclaimPolicy;
-                //         // query['resource_Sc_Allow_Volume_Expansion'] = result.items[i].allowVolumeExpansion;
-                //         // query['resource_Sc_Volume_Binding_Mode'] = result.items[i].volumeBindingMode;
-                //         // query['resource_Status'] = result.items[i].status; //object
                 //         query['resource_Level1'] = "OS"; //Openstack
                 //         query['resource_Level2'] = resourceType;
                 //         query['resource_Level_Type'] = "OX";  //Openstack-Cluster
@@ -826,7 +811,7 @@ async function connectQueue() {
                 //         query['resource_Active'] = true;
                 //         // query['resource_Status_Updated_At'] = new Date();
                 //
-                //         tempQuery = formatter_resource(i, itemLength, resourceType, cluster_uuid, query, mergedQuery);
+                //         tempQuery = formatter_resource(i, length, resourceType, cluster_uuid, query, mergedQuery);
                 //         mergedQuery = tempQuery;
                 //     }
                 //
@@ -857,7 +842,7 @@ async function connectQueue() {
                         query['resource_Anomaly_Monitor'] = false;
                         query['resource_Active'] = true;
 
-                        tempQuery = formatter_resource(i, itemLength, resourceType, cluster_uuid, query, mergedQuery);
+                        tempQuery = formatter_resource(i, length, resourceType, cluster_uuid, query, mergedQuery);
                         mergedQuery = tempQuery;
                     }
 
@@ -894,7 +879,7 @@ async function connectQueue() {
                         query['resource_Anomaly_Monitor'] = false;
                         query['resource_Active'] = true;
 
-                        tempQuery = formatter_resource(i, itemLength, resourceType, cluster_uuid, query, mergedQuery);
+                        tempQuery = formatter_resource(i, length, resourceType, cluster_uuid, query, mergedQuery);
                         mergedQuery = tempQuery;
                     }
 
