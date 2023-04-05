405866911ca473a7b50607ea0ffc072519c91ab2
jeongcm <jeongcm0101@gmail.com>
2023-04-05 17:45:19 +0900

    pjtask/1586 resource type 별 디렉터리 분개, getResionQuery 생성

--- src/resource/ncp/resource.js
+++ src/resource/ncp/resource.js
@@ -1,46 +1,19 @@
 import {v1}  from 'uuid';
+import {getRegionListQuery} from "./region/region.js";
 
 export async function getResourceQuery(totalMsg, clusterUuid) {
-    let resourceType;
-    let query = {};
-    let mergedQuery = {};
-    let tempQuery = {};
-    let resultLength = 0
-
+    let queryResult = {};
     let result = totalMsg.result
     switch (totalMsg.template_uuid) {
         case "70000000000000000000000000000001":
-            resourceType = "RG";
-            resultLength = result.getRegionListResponse?.regionList?.length
-            for (let i = 0; i < resultLength; i ++) {
-                query['resource_Type'] = resourceType;
-                query['resource_Spec'] = result.getRegionListResponse?.regionList[i];
-                query['resource_Group_Uuid'] = clusterUuid;
-                query['resource_Name'] = result.getRegionListResponse?.regionList[i]?.regionName;
-                query['resource_Description'] = "";
-                // query['resource_Instance'] =
-                query['resource_Target_Uuid'] = result.getRegionListResponse?.regionList[i]?.regionCode; // new generate target uuid
-                query['resource_Target_Created_At'] = new Date();
-                // query['resource_Namespace'] =
-                // query['parent_Resource_Id'] =
-                // query['resource_Status'] =
-                query['resource_Level1'] = "NCP";
-                query['resource_Level2'] = resourceType;
-                // query['resource_Level3'] = "";
-                query['resource_Level_Type'] = "NX";
-                query['resource_Rbac'] = false;
-                query['resource_Anomaly_Monitor'] = false;
-                query['resource_Active'] = true;
-                query['resource_Status_Updated_At'] = new Date();
-
-                tempQuery = formatter_resource(i, resultLength, resourceType, clusterUuid, query, mergedQuery);
-                mergedQuery = tempQuery;
-            }
+            queryResult = await getRegionListQuery(result, clusterUuid)
+        break;
     }
-    return { message: mergedQuery, resourceType: resourceType };
+
+    return queryResult;
 }
 
-function formatter_resource(i, itemLength, resourceType, cluster_uuid, query, mergedQuery) {
+export function formatter_resource(i, itemLength, resourceType, cluster_uuid, query, mergedQuery) {
     let interimQuery = {};
     try {
         if (itemLength==1) {
